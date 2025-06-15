87446c234a6a2abe132dc03a3bf27d842d30c5e4
wsalquinga <wilmer_alquinga@outlook.es>
2025-06-15 07:15:03 -0500

    feat: add method to map from entity to dto

--- backend/src/main/java/dev/walquinga/employee_ws/mappers/DepartmentMapper.java
+++ backend/src/main/java/dev/walquinga/employee_ws/mappers/DepartmentMapper.java
@@ -4,7 +4,13 @@ import dev.walquinga.employee_ws.dtos.DepartmentDto;
 import dev.walquinga.employee_ws.models.Department;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {
     Department toEntity(DepartmentDto dto);
+
+    DepartmentDto toDto(Department entity);
+
+    List<DepartmentDto> toDtos(List<Department> entities);
 }
--- backend/src/main/java/dev/walquinga/employee_ws/mappers/EmployeeMapper.java
+++ backend/src/main/java/dev/walquinga/employee_ws/mappers/EmployeeMapper.java
@@ -4,7 +4,13 @@ import dev.walquinga.employee_ws.dtos.EmployeeDto;
 import dev.walquinga.employee_ws.models.Employee;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
     Employee toEntity(EmployeeDto dto);
+
+    EmployeeDto toDto(Employee entity);
+
+    List<EmployeeDto> toDtos(List<Employee> entities);
 }
