1b5ef725ed13bbb889444f683308dffad00afb12
wsalquinga <wilmer_alquinga@outlook.es>
2025-06-15 06:03:18 -0500

    feat: add department and employee controllers and set api version property

---
+++ backend/src/main/java/dev/walquinga/employee_ws/controllers/EmployeeController.java
@@ -0,0 +1,42 @@
+package dev.walquinga.employee_ws.controllers;
+
+import dev.walquinga.employee_ws.dtos.EmployeeDto;
+import dev.walquinga.employee_ws.models.Employee;
+import dev.walquinga.employee_ws.services.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping("/{departmentId}")
+    public ResponseEntity<Employee> create(
+            @PathVariable Long departmentId,
+            @RequestBody EmployeeDto employeeDto
+    ) {
+        return ResponseEntity.ok(employeeService.create(departmentId, employeeDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        employeeService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> list() {
+        return ResponseEntity.ok(employeeService.list());
+    }
+}
