ba3f3391b1b73db310ab6c808933883eb64b029e
wsalquinga <wilmer_alquinga@outlook.es>
2025-06-16 04:12:43 -0500

    feat: add department model and services

---
+++ frontend/src/app/core/services/department.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {Department} from '../../models/department.model';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({providedIn: 'root'})
+export class DepartmentService {
+  private readonly API_URL = 'http://localhost:8080/api/v1';
+  private departments = new BehaviorSubject<Department[]>([]);
+  departments$ = this.departments.asObservable();
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): void {
+    this.http.get<Department[]>(`${this.API_URL}/department`).subscribe(response => this.departments.next(response));
+  }
+
+  create(name: string): Observable<Department> {
+    return this.http.post<Department>(`${this.API_URL}/department/create`, {name});
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.post<void>(`${this.API_URL}/department/delete/${id}`, {});
+  }
+}
