09f1af7e2410e679a146a8b8f2bde93f7d1975e7
wsalquinga <wilmer_alquinga@outlook.es>
2025-06-16 22:26:18 -0500

    feat: change services to use api url from environment file

--- frontend/src/app/core/services/department.service.ts
+++ frontend/src/app/core/services/department.service.ts
@@ -2,10 +2,11 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {Department} from '../../models/department.model';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class DepartmentService {
-  private readonly API_URL = 'http://localhost:8080/api/v1';
+  private readonly API_URL = `${environment.apiUrl}/department`;
   private departments = new BehaviorSubject<Department[]>([]);
   departments$ = this.departments.asObservable();
 
@@ -13,18 +14,18 @@ export class DepartmentService {
   }
 
   getAll(): void {
-    this.http.get<Department[]>(`${this.API_URL}/department`).subscribe(response => this.departments.next(response));
+    this.http.get<Department[]>(this.API_URL).subscribe(response => this.departments.next(response));
   }
 
   getAllActive(): void {
-    this.http.get<Department[]>(`${this.API_URL}/department/active`).subscribe(response => this.departments.next(response));
+    this.http.get<Department[]>(`${this.API_URL}/active`).subscribe(response => this.departments.next(response));
   }
 
   create(name: string): Observable<Department> {
-    return this.http.post<Department>(`${this.API_URL}/department/create`, {name});
+    return this.http.post<Department>(`${this.API_URL}/create`, {name});
   }
 
   delete(id: number): Observable<void> {
-    return this.http.post<void>(`${this.API_URL}/department/delete/${id}`, {});
+    return this.http.post<void>(`${this.API_URL}/delete/${id}`, {});
   }
 }
