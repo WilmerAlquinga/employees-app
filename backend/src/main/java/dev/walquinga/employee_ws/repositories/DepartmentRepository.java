936840b3c3b3becaca4e22aeb5dfc1b56d37c5fe
wsalquinga <wilmer_alquinga@outlook.es>
2025-06-16 20:52:00 -0500

    feat: update department and employee methods to get actives and rename methods

--- backend/src/main/java/dev/walquinga/employee_ws/repositories/DepartmentRepository.java
+++ backend/src/main/java/dev/walquinga/employee_ws/repositories/DepartmentRepository.java
@@ -1,7 +1,11 @@
 package dev.walquinga.employee_ws.repositories;
 
 import dev.walquinga.employee_ws.models.Department;
+import dev.walquinga.employee_ws.models.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    List<Department> findByStatus(Status status);
 }
